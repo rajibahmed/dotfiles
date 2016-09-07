e311828dbd23b83c49b3498465c205e7bb83000e
Rajib Ahmed <rajib@magplus.com>
2016-09-07 11:15:46 +0200

    Add basic setup

---
+++ setup_machine.sh
@@ -0,0 +1,8 @@
+echo "1. Setting Up homebrew"
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+
+brew install rbenv 
+brew install nvm
+brew install neovim/neovim/neovim
