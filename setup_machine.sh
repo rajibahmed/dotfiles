b60301824bd5fa236feb94511aaa8112af71c064
Rajib Ahmed <rajib@magplus.com>
2016-09-07 11:20:07 +0200

    Add basic installation

--- setup_machine.sh
+++ setup_machine.sh
@@ -3,6 +3,16 @@ echo "1. Setting Up homebrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 
+echo "2. Installing some useful things using brew"
 brew install rbenv 
 brew install nvm
 brew install neovim/neovim/neovim
+brew install ctags
+brew install tmux
+brew install zsh 
+brew install zsh-completions
+brew install fasd
+brew install git 
+brew install hub
+brew install imagemagick
+brew install wget 
