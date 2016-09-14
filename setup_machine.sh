68ec4cd5c234a2fa050b2200a095bc62d20c5344
Rajib Ahmed <rajib@magplus.com>
2016-09-14 10:31:57 +0200

    Add iterm2 via brew cask.

--- setup_machine.sh
+++ setup_machine.sh
@@ -16,6 +16,13 @@ brew install git
 brew install hub
 brew install imagemagick
 brew install wget 
+brew install python3
+brew install rbenv
+brew install go
+brew install the_silver_searcher
+brew install tree
+brew tap caskroom/cask
+brew install Caskroom/cask/iterm2
 
 
 echo "3. Installing vim-plug for neovim"
@@ -24,7 +31,9 @@ curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 
+echo "5. install powerline"
+pip3 install powerline-status
+
 
 echo "4. Change default shell"
-
 sudo chsh
