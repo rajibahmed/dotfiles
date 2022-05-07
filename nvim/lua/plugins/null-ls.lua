cf4d8a29829d8ef36234d579c0ee4073d89904d3
Rajib Ahmed <l.rajibahmed@gmail.com>
2022-05-07 13:09:01 +0200

    fix: rustfmt for format on save

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -3,7 +3,8 @@ require("null-ls").setup({
     sources = {
         require("null-ls").builtins.formatting.prettier,
         require("null-ls").builtins.formatting.eslint,
-        require("null-ls").builtins.completion.luasnip
+        require("null-ls").builtins.completion.luasnip,
+        require("null-ls").builtins.formatting.rustfmt
     },
 
     on_attach = function(client)
